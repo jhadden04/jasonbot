257710bc5e70eb202f7699b027596d323cef0f68
John Hadden <59323260+jhadden04@users.noreply.github.com>
2020-12-16 15:26:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
 import praw
 import time
+
 reddit = praw.Reddit(client_id='2WHqLvf5aRq3tQ',
                      client_secret='J1WHeJffj_QlK5pvom_qKPJUYsVA4Q',
                      user_agent='a reddit instance',
                      username='FranchescaScola',
-                     password='12qwaszxX@1zp')
+                     password='mnkjoi09M)')
+
 
 def subspam():
-    misctext = """Message from John, original owner of thcdelivery, https://www.realthcdelivery.com/
-
-Want to let you know that the REAL THC DELIVERY has extended its sale until this thursday:
+    misctext = """John here, original owner of thcdelivery, https://www.realthcdelivery.com/
+messaging you all of you to let you know that the
+REAL THC DELIVERY Will be doing a Friday Flash Sale :
 
 SITE WIDE 50% OFF SALE
 
-Use Coupon Code : RealTHCD At Checkout"""  # you need to change this to your message
+Use Coupon Code :
+RealTHCD
+At Checkout"""
 
     subname = 'CanadianMOMs+canadients'  # obviously you can add more subreddits to this
     usedusernames = []
-    for comment in reddit.subreddit(subname).stream.comments():
-        name = comment.author.name
-        title = f'Dear, {name}'
-        if name in usedusernames:
-            continue
-        try:
-            text = misctext
-            reddit.redditor(name).message(title, text)
-        except:
-            continue
-        usedusernames.append(name)
-        print(f'u/{name} r/{comment.subreddit.display_name}')
-        time.sleep(40)
+    while True:
+        for comment in reddit.subreddit(subname).stream.comments():
+            name = comment.author.name
+            title = f'Dear, {name}'
+            nametxt = f'{name}\n'
+            if name in usedusernames:
+                continue
+            b = open("names.txt", "r")
+            if nametxt in b:
+                continue
+            try:
+                text = misctext
+                reddit.redditor(name).message(title, text)
+            except:
+                continue
+            usedusernames.append(name)
+            f = open("names.txt", "a")
+            f.write(nametxt)
+            print(f'u/{name} r/{comment.subreddit.display_name}')
+            time.sleep(40)
+            break
+
+
 subspam()
